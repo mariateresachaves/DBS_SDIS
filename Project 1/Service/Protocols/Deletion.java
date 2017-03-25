f72b7ae7da66b9386b867c9eed9d2e06da4480ec
mariateresachaves <teresachavesrpg@gmail.com>
2017-03-25 20:28:44 +0000

    Only one message for all file chunks deletion

--- Project 1/Service/Protocols/Deletion.java
+++ Project 1/Service/Protocols/Deletion.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.logging.Level;
 
 import Service.Peer;
@@ -19,12 +20,17 @@ public class Deletion {
 	private static InetAddress address;
 
 	public Deletion(String filePathName) {
-		chunk_info = Peer.getStoredChunk(filePathName);
+		chunk_info = Peer.getStoredChunk(filePathName).get(0);
 		version = "1.0";
 	}
 
 	public void send_delete() throws IOException {
 		Util.getLogger().log(Level.INFO, "Sending DELETE to MC Channel");
+
+		// MDB Channel
+		hostname = Util.getProperties().getProperty("MC_IP");
+		port = Integer.parseInt(Util.getProperties().getProperty("MC_PORT"));
+		address = InetAddress.getByName(hostname);
 
 		// Create message to send
 		String tmp_msg = String.format("DELETE %s %s %s \r\n\r\n", version, chunk_info.getSenderID(),
@@ -34,11 +40,6 @@ public class Deletion {
 
 		// Socket to send the message
 		DatagramSocket socket = new DatagramSocket();
-
-		// MDB Channel
-		hostname = Util.getProperties().getProperty("MC_IP");
-		port = Integer.parseInt(Util.getProperties().getProperty("MC_PORT"));
-		address = InetAddress.getByName(hostname);
 
 		DatagramPacket packet = new DatagramPacket(msg, msg.length, address, port);
 		socket.send(packet);
