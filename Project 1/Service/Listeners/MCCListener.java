20c38ead1e66f1c47b3494c99daaa1fef7ac0ccb
pedro <you@example.com>
2017-03-26 04:43:57 -0700

    Added Control Channel Listener

---
+++ Project 1/Service/Listeners/MCCListener.java
@@ -0,0 +1,123 @@
+package Service.Listeners;
+
+import java.io.File;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.logging.Level;
+
+import Service.Protocols.Chunk;
+import Utils.Util;
+
+public class MCCListener implements Runnable {
+
+
+	private String channelIP;
+	private String channelport;
+
+	public MCCListener() {
+		// Variables assingment
+		channelIP = Util.getProperties().getProperty("MC_IP", "224.13.3.1");
+		channelport = Util.getProperties().getProperty("MC_PORT", "9176");
+
+	}
+
+	public void run() {
+
+		Util.getLogger().log(Level.INFO, "Starting Multicast Control Channel Listener");
+		// Set of variables
+		MulticastSocket mcast_socket = null;
+
+		try {
+			mcast_socket = new MulticastSocket(Integer.parseInt(this.channelport));
+			mcast_socket.joinGroup(InetAddress.getByName(this.channelIP));
+		} catch (Exception e) {
+			Util.getLogger().log(Level.SEVERE, "Error creating Listener for multicast Restore Channel");
+			System.exit(Util.ERR_CREATELISTMCC);
+		}
+
+		recieveMessage(mcast_socket);
+	}
+
+	private void recieveMessage(MulticastSocket sck) {
+		byte[] buf = new byte[4096];
+		DatagramPacket packet_received = new DatagramPacket(buf, buf.length);
+
+		try {
+			while (true) {
+
+				sck.receive(packet_received);
+
+				String response = new String(packet_received.getData());
+				String protocolMessage = processProtocol(response);
+
+				selectProtocol(protocolMessage);
+
+			}
+		} catch (Exception e) {
+			Util.getLogger().log(Level.WARNING, "Error Recieving packet, Error Message: ");
+			e.printStackTrace();
+		}
+
+	}
+
+	private void selectProtocol(String protocolMessage) {
+		switch (protocolMessage.split(" ")[0]) {
+
+		case "GETCHUNK":
+			getChunkProtocol();
+			break;
+		case "DELETE":
+			deleteProtocol();
+			break;
+		case "REMOVED":
+			removedProtocol();
+			break;
+		case "STORED":
+			storedProtocol();
+			break;
+
+
+		}
+	}
+
+
+
+	private void storedProtocol() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void removedProtocol() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void deleteProtocol() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void getChunkProtocol() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private String processProtocol(String response) {
+
+		String old = response;
+
+		String processed;
+		processed = old.replaceAll("  ", " ");
+		if (old.equalsIgnoreCase(processed)) {
+			return processed;
+		} else {
+			return processProtocol(processed);
+		}
+
+	}
+
+
+
+}
