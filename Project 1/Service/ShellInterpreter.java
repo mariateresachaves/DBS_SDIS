ba01b94c5246815e5e54aa1b9686b2c28d091eb3
pedro <you@example.com>
2017-04-10 04:04:57 -0700

    Adicionado prototipo para RMI

--- Project 1/Service/ShellInterpreter.java
+++ Project 1/Service/ShellInterpreter.java
@@ -2,6 +2,10 @@ package Service;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -19,11 +23,34 @@ import Service.Protocols.Reclaim;
 import Utils.Util;
 import Service.Protocols.SetDisk;
 
-public class ShellInterpreter {
+public class ShellInterpreter implements ConnectBackInterface {
 
 	private Scanner sc = new Scanner(System.in);
 
 	public ShellInterpreter() {
+
+		int useRMI = Integer.parseInt(Utils.Util.getProperties().getProperty(
+				"useRMI", "1"));
+		if (useRMI == 1) {
+
+			try {
+
+				ConnectBackInterface stub = (ConnectBackInterface) UnicastRemoteObject
+						.exportObject(this, 1098);
+				// Bind the remote object's stub in the registry
+				Registry registry = LocateRegistry.getRegistry();
+
+				registry.rebind(
+						Utils.Util.getProperties().getProperty("SenderID",
+								"Agent999"), stub);
+
+				Utils.Util.getLogger().log(Level.INFO, "RMI Server Started");
+			} catch (Exception e) {
+				Utils.Util.getLogger().log(Level.WARNING,
+						"RMI Server Failed to Start Started");
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public void getShell() throws Exception {
@@ -49,8 +76,10 @@ public class ShellInterpreter {
 		case "BACKUP":
 			// Incorrect number of arguments
 			if (args.length != 2) {
-				System.out.println("Usage: BACKUP <FilePathName> <ReplicationDegree>");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the BACKUP command\n");
+				System.out
+						.println("Usage: BACKUP <FilePathName> <ReplicationDegree>");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the BACKUP command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -62,7 +91,8 @@ public class ShellInterpreter {
 			// Incorrect number of arguments
 			if (args.length != 1) {
 				System.out.println("Usage: RESTORE <FilePathName>");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the RESTORE command\n");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the RESTORE command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -74,7 +104,8 @@ public class ShellInterpreter {
 			// Incorrect number of arguments
 			if (args.length != 1) {
 				System.out.println("Usage: DELETE <FilePathName>");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the DELETE command\n");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the DELETE command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -86,7 +117,8 @@ public class ShellInterpreter {
 			// Incorrect number of arguments
 			if (args.length != 0) {
 				System.out.println("Usage: RECLAIM");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the RECLAIM command\n");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the RECLAIM command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -98,7 +130,8 @@ public class ShellInterpreter {
 			// Incorrect number of arguments
 			if (args.length != 0) {
 				System.out.println("Usage: STATE");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the STATE command\n");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the STATE command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -109,8 +142,10 @@ public class ShellInterpreter {
 		case "SETDISK": // To define the disk space that can be used
 			// Incorrect number of arguments
 			if (args.length != 1) {
-				System.out.println("Usage: SETDISK <MaximumDiskSpace> [KBytes]");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the SETDISK command\n");
+				System.out
+						.println("Usage: SETDISK <MaximumDiskSpace> [KBytes]");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the SETDISK command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -123,7 +158,8 @@ public class ShellInterpreter {
 			Peer.xmldb.saveDatabase();
 			if (args.length != 0) {
 				System.out.println("Usage: QUIT");
-				Util.getLogger().log(Level.SEVERE, "Invalid arguments at the QUIT command\n");
+				Util.getLogger().log(Level.SEVERE,
+						"Invalid arguments at the QUIT command\n");
 			}
 			// Correct number of arguments
 			else {
@@ -148,7 +184,8 @@ public class ShellInterpreter {
 		try {
 			value = Integer.parseInt(args[0].trim());
 		} catch (NumberFormatException e) {
-			Util.getLogger().log(Level.WARNING, "Incorrect Number Format, aborting\n");
+			Util.getLogger().log(Level.WARNING,
+					"Incorrect Number Format, aborting\n");
 			return;
 		}
 
@@ -187,10 +224,12 @@ public class ShellInterpreter {
 			String senderId = split[4].trim();
 
 			if (Integer.parseInt(desiredRD) < Integer.parseInt(RD)) {
-				Util.getLogger().log(Level.INFO, "Deleting chunk No " + chunkNo + "\n");
+				Util.getLogger().log(Level.INFO,
+						"Deleting chunk No " + chunkNo + "\n");
 				MDBListener.deleteChunk(fileId, senderId, chunkNo);
 
-				Reclaim controller = new Reclaim(senderId, fileId, Integer.parseInt(chunkNo));
+				Reclaim controller = new Reclaim(senderId, fileId,
+						Integer.parseInt(chunkNo));
 				controller.send_removed();
 				break;
 			}
@@ -206,12 +245,13 @@ public class ShellInterpreter {
 
 	private void protoRestore(String[] args) throws IOException {
 		Util.getLogger().log(Level.INFO, "Running Restore Protocol\n");
-        Restore controller = new Restore(args[0]);
+		Restore controller = new Restore(args[0]);
 
-        controller.send_getchunk();
-        
-        System.out.println("Request Packets Sent, Waiting for replies, this could take a while, go grab a coffe");
-        controller.assemblyFile();
+		controller.send_getchunk();
+
+		System.out
+				.println("Request Packets Sent, Waiting for replies, this could take a while, go grab a coffe");
+		controller.assemblyFile();
 
 		// TODO:
 		/*
@@ -240,7 +280,8 @@ public class ShellInterpreter {
 
 		// Save on the database
 		if (!Peer.xmldb.isFilePresent(args[0], chunks.get(0).getFileID())) {
-			Peer.xmldb.addFile(args[0], chunks.get(0).getFileID(), chunks.get(0).getReplicationDegree() + "", "0");
+			Peer.xmldb.addFile(args[0], chunks.get(0).getFileID(), chunks
+					.get(0).getReplicationDegree() + "", "0");
 		}
 
 		int k = 1;
@@ -250,15 +291,18 @@ public class ShellInterpreter {
 			time = 1000;
 
 			while (!done && tries != 5) {
-				System.out.println("[ ~~~~~~ Try " + (tries + 1) + " ~~~~~~ ]\n");
+				System.out.println("[ ~~~~~~ Try " + (tries + 1)
+						+ " ~~~~~~ ]\n");
 				int i = chunk.getReplicationDegree();
 
 				while (i > 0) {
 					DatagramPacket packet = controller.make_packet(chunk);
 
 					// Adicionar part à base de dados
-					if (!Peer.xmldb.isPartPresent(args[0], chunks.get(0).getFileID(), chunk.getChunkNo()))
-						Peer.xmldb.addFilePart(args[0], chunks.get(0).getFileID(), chunk.getChunkNo(), 0);
+					if (!Peer.xmldb.isPartPresent(args[0], chunks.get(0)
+							.getFileID(), chunk.getChunkNo()))
+						Peer.xmldb.addFilePart(args[0], chunks.get(0)
+								.getFileID(), chunk.getChunkNo(), 0);
 
 					controller.send_putchunk(packet);
 
@@ -273,14 +317,20 @@ public class ShellInterpreter {
 				PacketCollector msgs = Peer.mccl.getCollectedMessages();
 
 				// Counts number of STOREs received
-				num_stores = msgs.numStores(chunk.getFileID(), chunk.getChunkNo() + "", time);
+				num_stores = msgs.numStores(chunk.getFileID(),
+						chunk.getChunkNo() + "", time);
 
-				if (Peer.xmldb.isPartPresent(args[0], chunks.get(0).getFileID(), chunk.getChunkNo()))
-					Peer.xmldb.updateFilePart(args[0], chunks.get(0).getFileID(), chunk.getChunkNo(), num_stores);
+				if (Peer.xmldb.isPartPresent(args[0],
+						chunks.get(0).getFileID(), chunk.getChunkNo()))
+					Peer.xmldb.updateFilePart(args[0], chunks.get(0)
+							.getFileID(), chunk.getChunkNo(), num_stores);
 
 				if (num_stores >= chunk.getReplicationDegree()) {
 					done = true;
-					Util.getLogger().log(Level.INFO, "Chunk No " + (chunk.getChunkNo() + 1) + " Stored Correctly\n");
+					Util.getLogger().log(
+							Level.INFO,
+							"Chunk No " + (chunk.getChunkNo() + 1)
+									+ " Stored Correctly\n");
 				}
 
 				// number of confirmation messages received lower than the
@@ -288,7 +338,10 @@ public class ShellInterpreter {
 				else {
 					// doubles the time interval for receiving confirmation
 					// messages
-					Util.getLogger().log(Level.INFO, "Going to try again with a period of " + time + "ms");
+					Util.getLogger().log(
+							Level.INFO,
+							"Going to try again with a period of " + time
+									+ "ms");
 					time = time * 2;
 					tries++;
 				}
@@ -297,5 +350,17 @@ public class ShellInterpreter {
 			tries = 0;
 			done = false;
 		}
+	}
+
+	@Override
+	public String sendCommand(String proto, String[] args)
+			throws RemoteException {
+		try {
+			this.inputCommand(proto, args);
+			return "Command Sent Successfully";
+		} catch (Exception e) {
+			return "Something went wrong processing the request";
+		}
+
 	}
 }
