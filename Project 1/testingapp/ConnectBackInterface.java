373e801b700c2128e60e6d26c62b7742a02b0478
pedro <you@example.com>
2017-04-10 05:51:52 -0700

    Connectback para testingapp

--- Project 1/testingapp/ConnectBackInterface.java
+++ Project 1/testingapp/ConnectBackInterface.java
@@ -1,6 +1,8 @@
+package Service;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ConnectBackInterface extends Remote {
-	String sendCommand(String b) throws RemoteException;
+	String sendCommand(String proto,String[] args) throws RemoteException;
 }
